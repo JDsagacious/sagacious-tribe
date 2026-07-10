7f2bc5a52caedffbdab8fe564366cdbd088d6ffb
John Dagogo Sanga <jdsagacious@proton.me>
2026-07-10 17:33:11 +0100

    Mission 013.2 - Fixed Ocean Guardian project structure

---
+++ js/ocean-guardian.js
@@ -0,0 +1,9 @@
+console.log("Ocean Guardian Loaded");
+
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", () => {
+
+    alert("Mission Starting Soon!");
+
+});
