b533e2b1a609866d16799e82e167653065da4e64
John Dagogo Sanga <jdsagacious@proton.me>
2026-07-19 07:52:07 +0100

    Ocean Guardian v1.0 - Fish animation and game improvements

--- js/ocean-guardian.js
+++ js/ocean-guardian.js
@@ -1,38 +1,36 @@
 console.log("Ocean Guardian Loaded");
 
 const ocean = document.getElementById("ocean");
-
+const trashLayer = document.getElementById("trashLayer");
 const scoreText = document.getElementById("score");
-
 const timerText = document.getElementById("timer");
-
 const startBtn = document.getElementById("startBtn");
 
 let score = 0;
-
 let gameRunning = false;
+let timeLeft = 60;
+let timerInterval;
 
 function createBottle(){
 
+    console.log("Bottle created");
+
     const bottle = document.createElement("img");
 
-bottle.src = "file:///C:/sagacious-tribe/assets/images/ocean-guardian/sprites/plastic-bottle.png";
+    bottle.src = "images/ocean-guardian/sprites/plastic-bottle.png";
+    console.log("Bottle path:", bottle.src);
 
-bottle.className = "bottle";
+    bottle.className = "bottle";
 
-    bottle.style.left =
-        Math.random()*85+"%";
-
-    bottle.style.top =
-        Math.random()*80+"%";
+    bottle.style.left = Math.random()*85 + "%";
+    bottle.style.top = Math.random()*80 + "%";
 
     bottle.onclick = function(){
 
         if(!gameRunning) return;
 
         score += 10;
-
-        scoreText.innerText = score;
+      scoreText.innerText = score;
 
         bottle.remove();
 
@@ -40,7 +38,7 @@ bottle.className = "bottle";
 
     };
 
-    ocean.appendChild(bottle);
+trashLayer.appendChild(bottle);
 
 }
 
@@ -51,16 +49,39 @@ startBtn.onclick = function(){
     gameRunning = true;
 
     score = 0;
+    timeLeft = 60;
 
     scoreText.innerText = score;
+    timerText.innerText = timeLeft;
 
-    ocean.innerHTML = "";
+    trashLayer.innerHTML = "";
 
-    for(let i=0;i<6;i++){
+    // Create 6 bottles
+    for(let i = 0; i < 6; i++){
 
         createBottle();
 
     }
+
+    clearInterval(timerInterval);
+
+    timerInterval = setInterval(function(){
+
+        timeLeft--;
+
+        timerText.innerText = timeLeft;
+
+        if(timeLeft <= 0){
+
+        clearInterval(timerInterval);
+
+            gameRunning = false;
+
+            alert("🌊 Mission Complete!\n\nEco Points: " + score);
+
+        }
+
+    },1000);
 
     alert("Mission Started!");
 
